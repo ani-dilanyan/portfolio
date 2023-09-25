c214223b4fb5d266da9524f858585b4b85a46e98
ani-dilanyan <anidilanyan05@gmail.com>
2023-09-25 23:23:43 +0400

    Create add_programmer view

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,5 @@ app_name = "main_app"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.ProgrammerDetailView.as_view(), name="details"),
-    # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("add_programmer", views.add_programmer, name="add_programmer"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
 
 from typing import Any
 from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 
@@ -24,3 +24,25 @@ class IndexView(generic.ListView):
 class ProgrammerDetailView(generic.DetailView):
     model = Programmer
     template_name = 'main_app/concret_programmer.html'
+
+
+def add_programmer(request):
+    if request.method == 'POST':
+        fname = request.POST["fname"]
+        lname = request.POST["lname"]
+        password = request.POST["password"]
+        age = request.POST["age"]
+        language = request.POST["language"]
+        framework = request.POST["framework"]
+        experience = request.POST["experience"]
+
+        user = User.objects.create_user(username = fname, first_name = fname, last_name = lname, password = password)
+        user.save()
+
+        programmer = Programmer(user = user, age = age, language = language, framework = framework, experience = experience)
+        programmer.save()
+
+        return redirect('http://localhost:8000/')
+    
+    return render(request, "main_app/add_programmer.html")
+
